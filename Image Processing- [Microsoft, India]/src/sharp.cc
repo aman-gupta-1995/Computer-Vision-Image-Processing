50e4e3acc3a0e37d1d882f67e5c1b9c82b0b8ae9
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-02-26 00:36:20 +0530

    Update sharp.cc

--- Image Processing- [Microsoft, India]/src/sharp.cc
+++ Image Processing- [Microsoft, India]/src/sharp.cc
@@ -1,7 +1,7 @@
 // Copyright 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 Lovell Fuller and contributors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License. 
+// you may not use this file except in compliance with the License.  
 // You may obtain a copy of the License at
 //   
 //     http://www.apache.org/licenses/LICENSE-2.0   
